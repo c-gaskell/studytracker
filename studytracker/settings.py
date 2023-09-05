6e750338ec1886e379f32052b2ee879ac72b952f
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2023-09-05 16:00:27 +0100

    Fix django_extensions

--- studytracker/settings.py
+++ studytracker/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-extensions'
-    'website'
+    'django_extensions',
+    'website',
 ]
 
 MIDDLEWARE = [
