293ece84bf4c6610fcbd7c908231bf1b003faedb
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2023-09-21 14:29:12 +0100

    Add ability to collapse multiple items

--- website/static/js/collapsible.js
+++ website/static/js/collapsible.js
@@ -4,15 +4,20 @@ function registerCollapsibles() {
 
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", function() {
-            var content = document.getElementById(this.getAttribute("for"));
-            if (!content.classList.contains("collapsible-hidden")) {
-                content.classList.add("collapsible-hidden");
+            var forArr = this.getAttribute("for").split(" ");
+            var content = forArr.map(document.getElementById, document);
+            console.log(content);
+
+            if (!content[0].classList.contains("collapsible-hidden")) {
+                for (var j = 0; j < content.length; j++) {
+                    content[j].classList.add("collapsible-hidden");
+                }
                 this.classList.add("collapse-button-hidden");
-                console.log("Hiding...");
             } else {
-                content.classList.remove("collapsible-hidden");
+                for (var j = 0; j < content.length; j++) {
+                    content[j].classList.remove("collapsible-hidden");
+                }
                 this.classList.remove("collapse-button-hidden");
-                console.log("Showing...");
             }
         });
     }
